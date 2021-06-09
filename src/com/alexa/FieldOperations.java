fd68b081af6dab5380b849217558c10c582a8c7a
Andrei Ciudacov <andrei.ciudacov@extendaretail.com>
2021-06-09 19:32:33 +0300

    added IncreaseField method

--- src/com/alexa/FieldOperations.java
+++ src/com/alexa/FieldOperations.java
@@ -5,11 +5,17 @@ import java.io.BufferedReader;
 import static java.lang.Integer.parseInt;
 
 public class FieldOperations {
-    public int[][] GetFieldFromFile(String file_path)
+
+    /**
+     *
+     * @param filePath File path from where to create sea battle field.
+     * @return Returns two-dimensional integer array filled according to file content with set dimensions.
+     */
+    public int[][] GetFieldFromFile(String filePath)
     {
         try
         {
-            BufferedReader reader = new BufferedReader(new java.io.FileReader(file_path));
+            BufferedReader reader = new BufferedReader(new java.io.FileReader(filePath));
             String line = reader.readLine();
             String[] fieldSize = line.split(" ");
             int x = parseInt(fieldSize[0]);
@@ -35,5 +41,33 @@ public class FieldOperations {
             e.printStackTrace();
             return null;
         }
+    }
+
+    /**
+     * @param sourceField Source sea battle field which will be increased.
+     * @param addRow If true, will add row. If false, will add column.
+     * @return Returns increased sea battle field.
+     * */
+    public int[][] IncreaseField(int[][] sourceField, boolean addRow)
+    {
+        int x = sourceField.length;
+        int y = sourceField[0].length;
+
+        int x_new = x;
+        int y_new = y;
+
+        if (addRow)
+            y_new++;
+        else
+            x_new++;
+
+        int[][] increasedField = new int[x_new][y_new];
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                increasedField[i][j] = sourceField[i][j];
+            }
+        }
+        return increasedField;
     }
 }
