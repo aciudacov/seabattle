c14228d3dea3433983317b6033e174565d665742
Andrei Ciudacov <andrei.ciudacov@extendaretail.com>
2021-06-10 08:37:05 +0300

    output array in cycle

--- src/com/alexa/Main.java
+++ src/com/alexa/Main.java
@@ -5,5 +5,21 @@ public class Main {
     public static void main(String[] args) {
         FieldOperations operations = new FieldOperations();
 	    int[][] field = operations.GetFieldFromFile("Corabii.in");
+
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[0].length; j++) {
+                System.out.print(field[i][j]);
+            }
+            System.out.println("\n");
+        }
+
+        field = operations.IncreaseField(field, true);
+        System.out.println("Increased by 1 row");
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[0].length; j++) {
+                System.out.print(field[i][j]);
+            }
+            System.out.println("\n");
+        }
     }
 }
