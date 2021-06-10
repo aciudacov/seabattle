a5fdcd62b1b8fb01ecda6be4c19ab1589a47a340
Andrei Ciudacov <andrei.ciudacov@extendaretail.com>
2021-06-10 15:27:13 +0300

    optimized & cleaned code

--- src/com/alexa/Main.java
+++ src/com/alexa/Main.java
@@ -5,23 +5,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        FieldOperations operations = new FieldOperations();
-	    int[][] field = operations.GetFieldFromFile("Corabii.in");
+        var scanner = new Scanner(System.in);
+        var operations = new FieldOperations();
+	    var field = operations.GetFieldFromFile("Corabii.in");
 
-        for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field[0].length; j++) {
+        for (int i = 0; i < field.length; i++)
+        {
+            for (int j = 0; j < field[0].length; j++)
+            {
                 System.out.print(field[i][j]);
             }
             System.out.println("\n");
         }
-        System.out.println("Enter type of operation, 1 if you want to add a column, 0 if a row");
-        int type = scanner.nextInt();
-        System.out.println("Your choice is read");
-        String str = scanner.nextLine();
-        field = operations.IncreaseField(field, type);
-        for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field[0].length; j++) {
+        System.out.println("Enter 1 if you want to add a column or any number for adding a row");
+        var type = scanner.nextInt();
+        field = operations.IncreaseField(field, type == 1 ? true : false);
+        
+        for (int i = 0; i < field.length; i++)
+        {
+            for (int j = 0; j < field[0].length; j++)
+            {
                 System.out.print(field[i][j]);
             }
             System.out.println("\n");
